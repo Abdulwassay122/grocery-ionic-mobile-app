11959c73dccb4c426649a7f0157612ce867f6183
Abdulwassay122 <aw122598@gmail.com>
2025-07-31 20:06:14 +0500

    1.0.7

--- src/main.ts
+++ src/main.ts
@@ -3,27 +3,27 @@ import { RouteReuseStrategy, provideRouter, withPreloading, PreloadAllModules } 
 import { IonicRouteStrategy, provideIonicAngular } from '@ionic/angular/standalone';
 import { routes } from './app/app.routes';
 import { AppComponent } from './app/app.component';
-import { addIcons } from 'ionicons';
-import { trashOutline, chevronBackOutline, searchOutline, mailOutline, notificationsOutline, locationOutline, chevronDownOutline, chevronForwardOutline, homeOutline, heartOutline, cartOutline, personOutline, addOutline, removeOutline } from 'ionicons/icons';
+// import { addIcons } from 'ionicons';
+// import { trashOutline, chevronBackOutline, searchOutline, mailOutline, notificationsOutline, locationOutline, chevronDownOutline, chevronForwardOutline, homeOutline, heartOutline, cartOutline, personOutline, addOutline, removeOutline } from 'ionicons/icons';
 import { provideHttpClient } from '@angular/common/http';
 import { PLATFORM_ID } from '@angular/core';
 
-addIcons({
-  'chevron-back-outline': chevronBackOutline,
-  'search-outline': searchOutline,
-  'mail-outline': mailOutline,
-  'notifications-outline': notificationsOutline,
-  'location-outline': locationOutline,
-  'chevron-down-outline': chevronDownOutline,
-  'chevron-forward-outline': chevronForwardOutline,
-  'heart-outline': heartOutline,
-  'home-outline': homeOutline,
-  'person-outline': personOutline,
-  'cart-outline': cartOutline,
-  'remove-outline': removeOutline,
-  'add-outline': addOutline,
-  'trash-outline': trashOutline,
-});
+// addIcons({
+//   'chevron-back-outline': chevronBackOutline,
+//   'search-outline': searchOutline,
+//   'mail-outline': mailOutline,
+//   'notifications-outline': notificationsOutline,
+//   'location-outline': locationOutline,
+//   'chevron-down-outline': chevronDownOutline,
+//   'chevron-forward-outline': chevronForwardOutline,
+//   'heart-outline': heartOutline,
+//   'home-outline': homeOutline,
+//   'person-outline': personOutline,
+//   'cart-outline': cartOutline,
+//   'remove-outline': removeOutline,
+//   'add-outline': addOutline,
+//   'trash-outline': trashOutline,
+// });
 bootstrapApplication(AppComponent, {
   providers: [
     { provide: RouteReuseStrategy, useClass: IonicRouteStrategy },
